e943e7e38ced1472fc53bb59fcff55467b6d9417
VaibhavBomle <bomlevaibhav@gmail.com>
2026-07-30 08:29:08 +0530

    space test push

--- week1/scrpper.py
+++ week1/scrpper.py
@@ -6,6 +6,7 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
 }
 
+
 def fetch_website_content(url): 
 
     respose = requests.get(url, headers=headers)
